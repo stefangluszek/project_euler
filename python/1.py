82800840c81f0858ce2afef47c337cc46cd8890c
Stefan Gluszek <gluszek.stefan@gmail.com>
2018-02-17 14:17:49 +0100

    Problem 1 - Python - use main function

--- python/1.py
+++ python/1.py
@@ -1,14 +1,18 @@
-import sys
+def sum_multiples_3_5(limit):
+  total = 0
 
-limit = int(sys.argv[1])
-
-total = 0
-
-for i in range(3, limit, 3):
-  total += i
-
-for i in range(5, limit, 5):
-  if (i % 3 != 0):
+  for i in range(3, limit, 3):
     total += i
 
-print str(total) 
+  for i in range(5, limit, 5):
+    if (i % 3 != 0):
+      total += i
+
+  return total
+
+
+def main():
+  print str(sum_multiples_3_5(1000))
+
+if __name__ == "__main__":
+  main()
