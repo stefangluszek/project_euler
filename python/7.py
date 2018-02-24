a000729d79aedaa1bec8431f2923c14efcf693a2
Stefan Gluszek <gluszek.stefan@gmail.com>
2018-02-24 10:43:43 +0100

    Problem 7 - Python.

---
+++ python/7.py
@@ -0,0 +1,7 @@
+import euler
+
+def main():
+  print str(euler.nth_prime(10001)) 
+
+if __name__ == "__main__":
+  main()
--- python/euler.py
+++ python/euler.py
@@ -1,5 +1,8 @@
+import math
+
 def find_n_primes(n):
-  """Returns a set containing all primes that are smaller than n.
+  """
+  Returns a set containing all primes that are smaller than n.
   NOTE: O(N log N)
   """
   primes = [ ]
@@ -18,3 +21,37 @@ def find_n_primes(n):
     if is_prime:
       primes.append(i)
   return primes
+
+def is_prime(n):
+  """Returns True if n is a prime, False otherwise"""
+  if n <= 1:
+    return False
+  if n < 4:
+    return True
+  if n % 2 == 0:
+    return False
+  if n < 9:
+    return True
+  if n % 3 == 0:
+    return False
+
+  limit = int(math.floor(math.sqrt(n)))
+  i = 5
+  while i <= limit:
+    if n % i == 0:
+      return False
+    if n % (i + 2) == 0:
+      return False
+    i += 6
+  return True
+
+def nth_prime(n):
+  if n == 1:
+    return 2
+  primes_found = 1
+  counter = 1
+  while primes_found < n:
+    counter += 2
+    if is_prime(counter) is True:
+      primes_found += 1
+  return counter
